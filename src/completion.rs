9a690bd525bc8a113157c37dd660e2d6dee295de
noahhhx <mrnoahgiles@gmail.com>
2026-09-22 08:09:01 +0100

    41

--- src/completion.rs
+++ src/completion.rs
@@ -21,14 +21,34 @@ struct CompletionCtx<'a> {
     word: &'a str,
     word_start: usize,
     command_position: bool,
+    command: String,
     first_arg_of: Option<String>,
+    previous_word: Option<String>,
 }
 
 fn context(line: &str) -> CompletionCtx<'_> {
     let word_start = line.rfind(char::is_whitespace).map_or(0, |i| i + 1);
     let word = &line[word_start..];
+
+    let prefix = &line[..word_start];
+    let prev_word_start = prefix
+        .trim()
+        .rfind(char::is_whitespace)
+        .map_or(0, |i| i + 1);
+    let previous_word = if prev_word_start != 0 {
+        let prev_word_start = &prefix[prev_word_start..];
+        Some(prev_word_start.trim().to_string())
+    } else {
+        None
+    };
+
     let command_position = word_start == 0;
     let parsed = parse(line);
+    let command: String = if parsed.is_empty() {
+        String::new()
+    } else {
+        parsed.clone().first().unwrap_or(&String::new()).clone()
+    };
     let first_arg_of = if !line.is_empty() && parsed.len() == 1 && line.ends_with(' ') {
         parsed.into_iter().next()
     } else {
@@ -38,7 +58,9 @@ fn context(line: &str) -> CompletionCtx<'_> {
         word,
         word_start,
         command_position,
+        command,
         first_arg_of,
+        previous_word,
     }
 }
 
@@ -47,12 +69,20 @@ pub fn complete(shell: &Shell, line: &str) -> CompletionAction {
     let ctx = context(line);
     if ctx.command_position {
         reduce(ctx.word_start, ctx.word, &mut exe_candidates(ctx.word))
-    } else if let Some(program) = ctx.first_arg_of
-        && let Some(script) = shell.completion_script(&program)
+    } else if let Some(program) = &ctx.first_arg_of
+        && let Some(script) = shell.completion_script(program)
     {
         CompletionAction::Replace {
             start: line.len(),
-            text: run_script(script),
+            text: run_script(script, ctx),
+            space: true,
+        }
+    } else if let Some(_previous_word) = &ctx.previous_word
+        && let Some(script) = shell.completion_script(&ctx.command)
+    {
+        CompletionAction::Replace {
+            start: line.len() - ctx.word.len(),
+            text: run_script(script, ctx),
             space: true,
         }
     } else {
@@ -84,10 +114,18 @@ fn reduce(word_start: usize, word: &str, candidates: &mut [String]) -> Completio
     }
 }
 
-fn run_script(cmd: &str) -> String {
+fn run_script(cmd: &str, ctx: CompletionCtx) -> String {
     let path = Path::new(cmd);
+    let first_arg = ctx.command;
+    let word = ctx.word.to_string();
+    let previosu_word = ctx.previous_word.unwrap_or_default();
+    let args = vec![first_arg, word, previosu_word.clone()];
+
     if path.is_file() && is_executable(path) {
-        let output = Command::new(path).output().expect("uh fucking oh");
+        let output = Command::new(path)
+            .args(args)
+            .output()
+            .expect("uh fucking oh");
         return String::from_utf8_lossy(output.stdout.trim_ascii_end()).to_string();
     }
     String::new()
@@ -201,30 +239,43 @@ fn common_prefix(candidates: &[String]) -> String {
     prefix.into_iter().collect()
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::common_prefix;
+#[cfg(test)]
+mod tests {
+    use crate::{completion::complete, shell::Shell};
 
-//     #[test]
-//     fn lcp_of_nested_prefixes() {
-//         let cands = ["xyz_foo", "xyz_foo_bar", "xyz_foo_bar_baz"].map(String::from);
-//         assert_eq!(common_prefix(&cands), "xyz_foo")
-//     }
+    use super::common_prefix;
 
-//     #[test]
-//     fn lcp_of_equal_length_matches() {
-//         let cands = ["xyz_foo", "xyz_cat", "xyz_zoo"].map(String::from);
-//         assert_eq!(common_prefix(&cands), "xyz_")
-//     }
+    #[test]
+    fn test_test() {
+        let mut shell = Shell::default();
+        let line = "git remote set";
+        shell
+            .completions
+            .insert("git".to_string(), "test".to_string());
 
-//     #[test]
-//     fn lcp_of_single_candidate() {
-//         let cands = [String::from("readme.txt")];
-//         assert_eq!(common_prefix(&cands), "readme.txt")
-//     }
+        complete(&shell, line);
+    }
 
-//     #[test]
-//     fn lcp_of_nothing() {
-//         assert_eq!(common_prefix(&[]), "")
-//     }
-// }
+    #[test]
+    fn lcp_of_nested_prefixes() {
+        let cands = ["xyz_foo", "xyz_foo_bar", "xyz_foo_bar_baz"].map(String::from);
+        assert_eq!(common_prefix(&cands), "xyz_foo")
+    }
+
+    #[test]
+    fn lcp_of_equal_length_matches() {
+        let cands = ["xyz_foo", "xyz_cat", "xyz_zoo"].map(String::from);
+        assert_eq!(common_prefix(&cands), "xyz_")
+    }
+
+    #[test]
+    fn lcp_of_single_candidate() {
+        let cands = [String::from("readme.txt")];
+        assert_eq!(common_prefix(&cands), "readme.txt")
+    }
+
+    #[test]
+    fn lcp_of_nothing() {
+        assert_eq!(common_prefix(&[]), "")
+    }
+}
